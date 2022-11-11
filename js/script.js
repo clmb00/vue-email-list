d7ed874ace2748212931609651be250bb0c80d0d
clmb00 <paolo.colombo2000@gmail.com>
2022-11-11 16:27:26 +0100

    [add] gets 10 mails from api

--- js/script.js
+++ js/script.js
@@ -3,10 +3,24 @@ const {createApp} = Vue;
 createApp({
   data(){
     return{
-
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      numberOfMails: 10,
+      mails: [],
+      isReady: false
     }
   },
   methods:{
-    
+    getMailFromApi(){
+      axios.get(this.apiUrl)
+       .then((mail)=>{
+        this.mails.push(mail.data.response);
+       })
+       .catch((error)=>{
+        this.mails.push(error.code);
+       })
+    },
+    doNcalls(n = this.numberOfMails){
+      for(i=0; i<n; i++) this.getMailFromApi();
+    }
   }
 }).mount('#app')

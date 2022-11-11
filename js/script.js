44348530b39754cb54d2822ab7135e75d8bd339d
clmb00 <paolo.colombo2000@gmail.com>
2022-11-11 16:50:48 +0100

    [add] loading while waiting

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ createApp({
       apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
       numberOfMails: 10,
       mails: [],
-      isReady: false
+      mailsReady: 0,
+      loading: false
     }
   },
   methods:{
@@ -14,13 +15,21 @@ createApp({
       axios.get(this.apiUrl)
        .then((mail)=>{
         this.mails.push(mail.data.response);
+        this.mailsReady++
        })
        .catch((error)=>{
         this.mails.push(error.code);
+        this.mailsReady++
        })
     },
     doNcalls(n = this.numberOfMails){
+      this.mailsReady = 0;
       for(i=0; i<n; i++) this.getMailFromApi();
+    },
+    clear(){
+      this.mails = [];
+      this.loading = false;
+      this.mailsReady = 0;
     }
   }
 }).mount('#app')
